92ddd0031a8dc537f29dc8aa163dce109621ec0c
Arpit <arpityadav666.ay@gmail.com>
2024-09-04 00:12:11 +0530

    added quote to hero-section

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -19,7 +19,7 @@ const HeroSection = () => {
         <div className="flex flex-col gap-3 my-6">
             <span className="mx-auto px-4  py-2 rounded-full bg-gray-100 font-medium text-red-600">No.1 Job Hunt Site</span>
             <h1 className="text-4xl font-semibold">Search, Apply & <br />Get Your <span className="text-purple-700">Dream Job</span> </h1>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque assumenda voluptatibus minus ea tempore nulla error aliquid quaerat veniam aut.</p>
+            <p>"Everyone has something to contribute to this world. It's just a matter of being given that opportunity to do so."</p>
         </div>
         <div className="flex w-[40%] shadow-md rounded-full border border-gray-200 pl-3 items-center gap-4 mx-auto">
             <input type="text" onChange={(e)=>{setQuery(e.target.value)}} placeholder="Find your dream job" className="outline-none border-none w-full" ></input>
